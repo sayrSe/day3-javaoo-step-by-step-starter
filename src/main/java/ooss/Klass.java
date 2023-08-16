bd8b22c093924d89bca711fb478d918935295564
Jose Angelo Arboleda <joseangelo.arboleda@gmail.com>
2023-08-16 18:45:41 +0800

    feat: should_return_false_when_equals_given_two_klass_with_different_id

--- src/main/java/ooss/Klass.java
+++ src/main/java/ooss/Klass.java
@@ -2,4 +2,24 @@ package ooss;
 
 public class Klass {
 
+    private final int id;
+
+    public Klass(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Klass klass = (Klass) o;
+
+        return id == klass.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
 }
