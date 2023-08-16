9abd15724f6b78f5b8c86a29c3c66a1ffd546210
Jose Angelo Arboleda <joseangelo.arboleda@gmail.com>
2023-08-16 17:14:13 +0800

    feat: should_return_is_equals_by_id_when_equals_given_two_person_with_id

--- src/main/java/ooss/Person.java
+++ src/main/java/ooss/Person.java
@@ -1,7 +1,6 @@
 package ooss;
 
 public class Person {
-
     private final int id;
     private final String name;
     private final int age;
@@ -14,5 +13,20 @@ public class Person {
 
     public String introduce() {
         return String.format("My name is %s. I am %d years old.", name, age);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        return id == person.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
     }
 }
