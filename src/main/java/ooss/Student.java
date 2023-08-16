299018f5e2625e936ac0605d8a31cbab1b3f697f
Jose Angelo Arboleda <joseangelo.arboleda@gmail.com>
2023-08-16 18:56:40 +0800

    feat: Should_only_in_the_recently_joined_class_when_isIn_given_student_join_two_classes_in_turn

--- src/main/java/ooss/Student.java
+++ src/main/java/ooss/Student.java
@@ -2,8 +2,18 @@ package ooss;
 
 public class Student extends Person {
 
+    private Klass klass;
+
     public Student(int id, String name, int age) {
         super(id, name, age);
         super.setOccupation("student");
+    }
+
+    public void join(Klass klass) {
+        this.klass = klass;
+    }
+
+    public boolean isIn(Klass klass) {
+        return klass.equals(this.klass);
     }
 }
