76686fef810b9050ac8f3907bcf2560a4c2dbd69
Jose Angelo Arboleda <joseangelo.arboleda@gmail.com>
2023-08-16 17:09:34 +0800

    feat: should_return_message_with_name_and_age_when_introduce_given_person

--- src/main/java/ooss/Student.java
+++ src/main/java/ooss/Student.java
@@ -2,4 +2,7 @@ package ooss;
 
 public class Student extends Person {
 
+    public Student(int id, String name, int age) {
+        super(id, name, age);
+    }
 }
--- src/main/java/ooss/Teacher.java
+++ src/main/java/ooss/Teacher.java
@@ -6,4 +6,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Teacher extends Person {
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
 }
